143607d08d3100dbf36b1dac28c1a377cf161447
Kenneth Hansen <kenneth@khansen-it.dk>
2020-08-24 21:56:57 +0200

    Initial commit of new website

---
+++ components/global/Navbar.vue
@@ -0,0 +1,81 @@
+<template>
+    <div class="relative bg-white">
+        <div class="max-w-7xl mx-auto px-4 sm:px-6">
+            <div class="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
+                <div class="lg:w-0 lg:flex-1">
+                    <nuxt-link to="/" class="flex">
+                        MailGuardian.org
+                    </nuxt-link>
+                </div>
+                <div class="-mr-2 -my-2 md:hidden">
+                    <button type="button" @click="mobileMenuOpen = true" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
+                    <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+                    </svg>
+                    </button>
+                </div>
+                <nav class="hidden md:flex space-x-10">
+                    <nuxt-link to="/releases" class="text-base leading-6 font-medium text-gray-500 hover:text-gray-900 focus:outline-none focus:text-gray-900 transition ease-in-out duration-150">
+                        Releases
+                    </nuxt-link>
+
+                    <nuxt-link to="/docs" class="text-base leading-6 font-medium text-gray-500 hover:text-gray-900 focus:outline-none focus:text-gray-900 transition ease-in-out duration-150">
+                        Docs
+                    </nuxt-link>
+                </nav>
+                <div class="hidden md:flex items-center justify-end space-x-8 md:flex-1 lg:w-0"></div>
+            </div>
+        </div>
+        <div class="absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden" v-show="mobileMenuOpen">
+            <div class="rounded-lg shadow-lg">
+                <div class="rounded-lg shadow-xs bg-white divide-y-2 divide-gray-50">
+                    <div class="pt-2 pb-6 px-2 space-y-6">
+                        <div class="flex items-center justify-between">
+                            <nuxt-link to="/">
+                                MailGuardian.org
+                            </nuxt-link>
+                            <div class="-mr-2">
+                                <button type="button" @click="mobileMenuOpen = false" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
+                                    <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                                    </svg>
+                                </button>
+                            </div>
+                        </div>
+                        <div>
+                            <nav class="grid row-gap-8">
+                                <nuxt-link to="/releases" class="p-3 flex items-center space-x-3 rounded-md hover:bg-gray-50 transition ease-in-out duration-150">
+                                    <svg class="flex-shrink-0 h-6 w-6 text-blue-600" fill="none" viewBox="0 0 20 20" stroke="currentColor">
+                                        <path d="M13 7H7v6h6V7z"></path>
+                                        <path fill-rule="evenodd" d="M7 2a1 1 0 012 0v1h2V2a1 1 0 112 0v1h2a2 2 0 012 2v2h1a1 1 0 110 2h-1v2h1a1 1 0 110 2h-1v2a2 2 0 01-2 2h-2v1a1 1 0 11-2 0v-1H9v1a1 1 0 11-2 0v-1H5a2 2 0 01-2-2v-2H2a1 1 0 110-2h1V9H2a1 1 0 010-2h1V5a2 2 0 012-2h2V2zM5 5h10v10H5V5z" clip-rule="evenodd"></path>
+                                    </svg>
+                                    <div class="text-base leading-6 font-medium text-gray-900">
+                                        Releases
+                                    </div>
+                                </nuxt-link>
+                                <nuxt-link to="/docs" class="p-3 flex items-center space-x-3 rounded-md hover:bg-gray-50 transition ease-in-out duration-150">
+                                    <svg class="flex-shrink-0 h-6 w-6 text-blue-600" fill="none" viewBox="0 0 20 20" stroke="currentColor">
+                                        <path d="M9 4.804A7.968 7.968 0 005.5 4c-1.255 0-2.443.29-3.5.804v10A7.969 7.969 0 015.5 14c1.669 0 3.218.51 4.5 1.385A7.962 7.962 0 0114.5 14c1.255 0 2.443.29 3.5.804v-10A7.968 7.968 0 0014.5 4c-1.255 0-2.443.29-3.5.804V12a1 1 0 11-2 0V4.804z"></path>
+                                    </svg>
+                                    <div class="text-base leading-6 font-medium text-gray-900">
+                                        Docs
+                                    </div>
+                                </nuxt-link>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return{
+            mobileMenuOpen: false
+        }
+    }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,182 @@
+<template>
+    <div>
+        <div class="py-12 bg-white">
+            <div class="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8">
+                <div class="lg:text-center">
+                    <h3 class="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10">
+                        A better way to protect your inbox
+                    </h3>
+                    <p class="mt-4 max-w-2xl text-xl leading-7 text-gray-500 lg:mx-auto">
+                        Secure your email inbox with a set of open-source tools protect you, or your company, from spam, phishing and malware
+                    </p>
+                </div>
+
+                <div class="mt-10">
+                    <ul class="md:grid md:grid-cols-2 md:col-gap-8 md:row-gap-10">
+                        <li>
+                            <div class="flex">
+                                <div class="flex-shrink-0">
+                                    <div class="flex items-center justify-center h-12 w-12 rounded-md bg-blue-500 text-white">
+                                        <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9" />
+                                        </svg>
+                                    </div>
+                                </div>
+                                <div class="ml-4">
+                                    <h4 class="text-lg leading-6 font-medium text-gray-900">Easy to use</h4>
+                                    <p class="mt-2 text-base leading-6 text-gray-500">
+                                        MailGuardian has a simple and easy to use web interface. The interface is built with cross device usability in mind so that it will work on your smartphone, tablet and desktop computer.
+                                    </p>
+                                </div>
+                            </div>
+                        </li>
+                        <li class="mt-10 md:mt-0 md:pb-10">
+                            <div class="flex">
+                                <div class="flex-shrink-0">
+                                    <div class="flex items-center justify-center h-12 w-12 rounded-md bg-blue-500 text-white">
+                                        <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 6l3 1m0 0l-3 9a5.002 5.002 0 006.001 0M6 7l3 9M6 7l6-2m6 2l3-1m-3 1l-3 9a5.002 5.002 0 006.001 0M18 7l3 9m-3-9l-6-2m0-2v2m0 16V5m0 16H9m3 0h3" />
+                                        </svg>
+                                    </div>
+                                </div>
+                                <div class="ml-4">
+                                    <h4 class="text-lg leading-6 font-medium text-gray-900">Solid technologies</h4>
+                                    <p class="mt-2 text-base leading-6 text-gray-500">
+                                        Unlike other frontends for the popular MailScanner application, MailGuardian is built with Python and PostgreSQL to allow growth and easy scalability.
+                                    </p>
+                                </div>
+                            </div>
+                        </li>
+                        <li class="mt-10 md:mt-0 md:pb-10">
+                            <div class="flex">
+                                <div class="flex-shrink-0">
+                                    <div class="flex items-center justify-center h-12 w-12 rounded-md bg-blue-500 text-white">
+                                        <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 10V3L4 14h7v7l9-11h-7z" />
+                                        </svg>
+                                    </div>
+                                </div>
+                                <div class="ml-4">
+                                    <h4 class="text-lg leading-6 font-medium text-gray-900">Best in class</h4>
+                                    <p class="mt-2 text-base leading-6 text-gray-500">
+                                        MailGuardian works as a management platform on top of popular and stable open-source tools like MailScanner and SpamAssassin. By building on top of these great tools, we make it possible to easily customize and extend your system to suit your specific needs.
+                                    </p>
+                                </div>
+                            </div>
+                        </li>
+                        <li class="mt-10 md:mt-0 md:pb-10">
+                            <div class="flex">
+                                <div class="flex-shrink-0">
+                                    <div class="flex items-center justify-center h-12 w-12 rounded-md bg-blue-500 text-white">
+                                        <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z" />
+                                        </svg>
+                                    </div>
+                                </div>
+                                <div class="ml-4">
+                                    <h4 class="text-lg leading-6 font-medium text-gray-900">RESTful API</h4>
+                                    <p class="mt-2 text-base leading-6 text-gray-500">
+                                        Easily integrate MailGuardian with your existing business applications, or management software, using our secure RESTful Web-API.
+                                    </p>
+                                </div>
+                            </div>
+                        </li>
+                        <li class="mt-10 md:mt-0 md:pb-10">
+                            <div class="flex">
+                                <div class="flex-shrink-0">
+                                    <div class="flex items-center justify-center h-12 w-12 rounded-md bg-blue-500 text-white">
+                                        <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z" />
+                                        </svg>
+                                    </div>
+                                </div>
+                                <div class="ml-4">
+                                    <h4 class="text-lg leading-6 font-medium text-gray-900">Scalable by default</h4>
+                                    <p class="mt-2 text-base leading-6 text-gray-500">
+                                        MailGuardian was built from the ground up to be highly scalable and has built in features for businesses to brand the application as their own.
+                                    </p>
+                                </div>
+                            </div>
+                        </li>
+                        <li class="mt-10 md:mt-0 md:pb-10">
+                            <div class="flex">
+                                <div class="flex-shrink-0">
+                                    <div class="flex items-center justify-center h-12 w-12 rounded-md bg-blue-500 text-white">
+                                        <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z" />
+                                        </svg>
+                                    </div>
+                                </div>
+                                <div class="ml-4">
+                                    <h4 class="text-lg leading-6 font-medium text-gray-900">Multiple access levels</h4>
+                                    <p class="mt-2 text-base leading-6 text-gray-500">
+                                        We have built MailGuardian as an application that works both your internal use or as the platform for a commercial email security service.
+                                    </p>
+                                </div>
+                            </div>
+                        </li>
+                        <li class="mt-10 md:mt-0 md:pb-10">
+                            <div class="flex">
+                                <div class="flex-shrink-0">
+                                    <div class="flex items-center justify-center h-12 w-12 rounded-md bg-blue-500 text-white">
+                                        <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z" />
+                                        </svg>
+                                    </div>
+                                </div>
+                                <div class="ml-4">
+                                    <h4 class="text-lg leading-6 font-medium text-gray-900">Manage multiple systems</h4>
+                                    <p class="mt-2 text-base leading-6 text-gray-500">
+                                        Due to the scalability of MailGuardian, it is possible to easily and convenient deploy and manage multiple hosts running the MailScanner engine and managed by MailGuardian.
+                                    </p>
+                                </div>
+                            </div>
+                        </li>
+                        <li class="mt-10 md:mt-0 md:pb-10">
+                            <div class="flex">
+                                <div class="flex-shrink-0">
+                                    <div class="flex items-center justify-center h-12 w-12 rounded-md bg-blue-500 text-white">
+                                        <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z" />
+                                        </svg>
+                                    </div>
+                                </div>
+                                <div class="ml-4">
+                                    <h4 class="text-lg leading-6 font-medium text-gray-900">Fully open-source</h4>
+                                    <p class="mt-2 text-base leading-6 text-gray-500">
+                                        MailGuardian is fully open-source under the GPL V2 license and all sourcecode is available on our GitHub page.
+                                    </p>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="bg-gray-50">
+            <div class="max-w-screen-xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
+                <h2 class="text-3xl leading-9 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10">
+                    Ready to start?
+                    <br>
+                    <span class="text-blue-600">Start right here!</span>
+                </h2>
+                <div class="mt-8 flex lg:flex-shrink-0 lg:mt-0">
+                    <div class="inline-flex rounded-md shadow">
+                        <nuxt-link to="/" class="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-blue-600 hover:bg-blue-500 focus:outline-none focus:shadow-outline transition duration-150 ease-in-out">
+                        Get started
+                        </nuxt-link>
+                    </div>
+                    <div class="ml-3 inline-flex rounded-md shadow">
+                        <nuxt-link to="/" class="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-blue-600 bg-white hover:text-blue-500 focus:outline-none focus:shadow-outline transition duration-150 ease-in-out">
+                        Learn more
+                        </nuxt-link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {}
+</script>
