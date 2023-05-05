66520f8650918fb67b71cb60fccf011860191b7d
Kenneth Hansen <kenneth@khansen-it.dk>
2023-05-05 22:16:28 +0200

    Library updates

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
-import { defineNuxtConfig } from 'nuxt'
+import { defineNuxtConfig } from 'nuxt/config'
 
-// https://v3.nuxtjs.org/api/configuration/nuxt.config
+// https://nuxt.com/docs/guide/directory-structure/nuxt.config
 export default defineNuxtConfig({
     modules: [
         '@nuxt/content',
